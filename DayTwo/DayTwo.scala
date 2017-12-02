59fb9e5384db366fe35d3d3babb01ef471f373b5
James Collerton <jamescollerton@live.co.uk>
2017-12-02 13:29:15 +0000

    In the middle of making it more scala like

--- DayTwo/DayTwo.scala
+++ DayTwo/DayTwo.scala
@@ -8,24 +8,23 @@ object DayTwo {
 	def calculateChecksum(rows: ArrayBuffer[Array[Int]]): Int = {
 		if(rows.size == 0) return 0
 		val currRow = rows(0)
-		var divided = currRow(0)
-		var divisor = currRow(0)
+		for (
+			i <- currRow
+			j <- currRow
+		) {
+
+		}
 		for(i <- currRow){
 			for(j <- currRow){
 				if(i % j == 0 && i != j){
-					divided = i;
-					divisor = j;	
+				return calculateChecksum(rows.slice(1, rows.length)) + i / j
 				}
 			}
 		}
-		println("Row " + currRow.mkString(" "))
-		println("Divided " + divided)
-		println("Divisor " + divisor)
-		println()
-		calculateChecksum(rows.slice(1, rows.length)) + divided / divisor
 	}
 
 	def main(args: Array[String]): Unit = {
+		// Answer is 233
 		val rows = Utils.readIn("DayTwoInput.txt");
 		println("Checksum " + calculateChecksum(rows));
 	}	
