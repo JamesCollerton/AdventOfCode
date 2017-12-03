81b5aee51ed72adb49e60fef19590a42db6d8a91
James Collerton <jamescollerton@live.co.uk>
2017-12-03 09:31:08 +0000

    Did first part

--- DayThree/DayThree.scala
+++ DayThree/DayThree.scala
@@ -1,7 +1,21 @@
 object DayThree {
 
-	def main(args: Array[String]): Unit = {
+	// Each number in the bottom right hand side of the
+	// square is a square number. We can use this to calculate
+	// the coordinates of the input (325489) and then just
+	// calculate the manhattan distance from there.
 
+	// Examples:
+	// 12: (4, 3) to (2, 2) on a 5 x 5 grid, answer: 3
+	// 23: (2, 0) to (2, 2) on a 5 x 5 grid, answer: 2
+	// 1024: (1, 32) to (16, 16) on a 33 x 33 grid, answer: 31
+	// 325489: (16, 0) to (284, 284) on a 571 x 571 grid, answer: 31
+
+	def main(args: Array[String]): Unit = {
+		println("12: " + SolveGrid.calculateDistance((4, 3), (2, 2)))
+		println("23: " + SolveGrid.calculateDistance((2, 0), (2, 2)))
+		println("1024: " + SolveGrid.calculateDistance((1, 32), (16, 16)))
+		println("325489: " + SolveGrid.calculateDistance((16, 0), (284, 284)))
 	}		
 
 }
--- DayThree/SolveGrid.scala
+++ DayThree/SolveGrid.scala
@@ -1,14 +1,9 @@
+import math._
+
 object SolveGrid {
 
-	// Each number in the bottom right hand side of the
-	// square is a square number. We can use this to calculate
-	// the coordinates of the input (325489) and then just
-	// calculate the manhattan distance from there.
-
-	// Examples:
-	// 12: (4, 3) to (2, 2) on a 5 x 5 grid
-	// 23: (2, 0) to (2, 2) on a 5 x 5 grid
-	// 1024: (1, 32) to (16, 16) on a 33 x 33 grid
-	// 325489: (16, 0) to (285, 285) on a 571 x 571 grid
+	def calculateDistance(start: (Int, Int), end: (Int, Int)): Int = {
+		Math.abs(start._1 - end._1) + Math.abs(start._2 - end._2)
+	}
 
 }
