0e0108aa2a5c5b44e420ba6716b8eeeb76aca1f9
James Collerton <James.Collerton@kobaltmusic.com>
2017-12-07 17:58:59 +0000

    Got a basic version working but misunderstood the problem

--- DaySeven/DaySeven.scala
+++ DaySeven/DaySeven.scala
@@ -5,12 +5,13 @@ import scala.collection.mutable.HashMap
 object DaySeven {
 
 	def main(args: Array[String]): Unit = {
-		val programs = Utils.readIn("DaySevenTestInput.txt")
-		// findBottom(programs)
-                findWeights(programs)
+		val programs = Utils.readIn("DaySevenInput.txt")
+		val bottom = findBottom(programs)
+                println("Bottom is " + bottom)
+                findWeights(programs, bottom)
 	}
 
-	def findBottom(programs: HashMap[String, Array[String]]): Unit = {
+	def findBottom(programs: HashMap[String, Array[String]]): String = {
 
 		// Find all supporting ones
 		val supportingPrograms = programs.filter(prog => prog._2.length > 0).keySet.toArray.map(name => name.split(" ")(0))
@@ -19,11 +20,11 @@ object DaySeven {
 		val supportedPrograms = programs.filter(prog => prog._2.length > 0).map(prog => prog._2).flatten
 
 		// The one that is supporting but is not supported is bottom
-		println("Bottom is " + (supportingPrograms.toSet.filterNot(supportedPrograms.toSet)))
+		supportingPrograms.toSet.filterNot(supportedPrograms.toSet).head.toString
 
 	}
 
-        def findWeights(programs: HashMap[String, Array[String]]): Unit = {
+        def findWeights(programs: HashMap[String, Array[String]], bottom: String): Unit = {
     
                 // Create HashMap of individual weights
                 val programWeights: HashMap[String, Int] = programs.map{ 
@@ -32,11 +33,22 @@ object DaySeven {
                     }
                 }
 
-                programWeights.foreach(x => println(x._1 + " -> " + x._2))
-
                 // Create HashMap of branches
-//                val programBranches: HashMap[String, Array[String]] =
+                val programBranches: HashMap[String, Array[String]] = programs.map{ 
+                    case (k, v) => (k.split(" ")(0), v)
+                }
+                
+                //println("Results for " + programBranches(bottom).map(prog => recurseStep(prog, programWeights, programBranches)).mkString(" "))
+                val bottomBranch = programBranches(bottom).map(prog => (prog, programWeights(prog), recurseStep(prog, programWeights, programBranches)))
+                val majorityValue = bottomBranch.map(x => x._3).groupBy(identity).maxBy(_._2.size)._1
+                val oddBranch = bottomBranch.filter(x => x._3 != majorityValue) //.map(x => x._2 + (majorityValue - x._3))
+                println(oddBranch(0)._1 + " " + oddBranch(0)._2  + " " + oddBranch(0)._3)
+                //println("New value " + oddBranch.mkString(""))
 
+        }
+
+        def recurseStep(bottom: String, programWeights: HashMap[String, Int], programBranches: HashMap[String, Array[String]]): Int = {
+              programBranches(bottom).map(prog => recurseStep(prog, programWeights, programBranches)).sum + programWeights(bottom)
         }
 
 }
