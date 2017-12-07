4e99e8427675324fcafade361fb181889d0a69b3
James Collerton <jamescollerton@live.co.uk>
2017-12-07 08:34:00 +0000

    Finished part one

--- DaySeven/DaySeven.scala
+++ DaySeven/DaySeven.scala
@@ -1,9 +1,27 @@
 import Utils._
 
+import scala.collection.mutable.HashMap
+
 object DaySeven {
 
 	def main(args: Array[String]): Unit = {
-		Utils.readIn("DaySevenTestInput.txt")		
+		val programs = Utils.readIn("DaySevenInput.txt")
+		findBottom(programs)
 	}
+
+	def findBottom(programs: HashMap[String, Array[String]]): Unit = {
+
+		// Find all supporting ones
+		val supportingPrograms = programs.filter(prog => prog._2.length > 0).keySet.toArray.map(name => name.split(" ")(0))
+
+		// Find all supported ones
+		val supportedPrograms = programs.filter(prog => prog._2.length > 0).map(prog => prog._2).flatten
+
+		// The one that is supporting but is not supported is bottom
+		println("Bottom is " + (supportingPrograms.toSet.filterNot(supportedPrograms.toSet)))
+
+	}
+
+	
 
 }
