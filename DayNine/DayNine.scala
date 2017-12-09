fbec199aa4efe71adada3aa70fe1eae22e8d6d45
James Collerton <jamescollerton@live.co.uk>
2017-12-09 12:38:34 +0000

    Cleaned up

--- DayNine/DayNine.scala
+++ DayNine/DayNine.scala
@@ -6,15 +6,15 @@ object DayNine {
 
 	def main(args: Array[String]): Unit = {
 		
-		// Testing
-		val fileNumberList = List("One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight")
-		fileNumberList.foreach(num => runOne("DayNineTestInput" + num + ".txt"));
+		// Testing One
+		//val fileNumberList = List("One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight")
+		//fileNumberList.foreach(num => runOne("DayNineTestInput" + num + ".txt"));
 
 		// Solution Two (now returns garbage score)
-		runOne("DayNineInput.txt")
+		getScore("DayNineInput.txt")
 	}
 
-	def runOne(inputFile: String): Unit = {
+	def getScore(inputFile: String): Unit = {
 		def inputStream = Utils.readIn(inputFile)(0)(0)
 		println("Score: " + calculateScore(inputStream))
 	}
@@ -30,25 +30,22 @@ object DayNine {
 		var bracketStack = List[String]()
 
 		inputStream.foreach(ch => {
-			if(skip){
-				skip = false;
-			} else if(ch == '<' && !garbage) {
-				garbage = true
-			} else if(ch == '{' && !garbage) {
-				bracketStack ::= "{"
-			} else if(ch == '!' && garbage) {
-				 skip = true
-			} else if(ch == '>' && garbage) {
-				garbage = false
-			} else if(ch == '}' && !garbage) {
-				score += bracketStack.length
-				bracketStack = bracketStack.tail
-			} else if (garbage) {
-				garbageScore += 1
-			}
+			if(skip) { skip = false }
+			else if(!garbage) {
+				if(ch == '<') 		{ garbage = true } 
+				else if(ch == '{') 	{ bracketStack ::= "{" } 
+				else if(ch == '}') 	{
+					score += bracketStack.length
+					bracketStack = bracketStack.tail
+				}
+			} else if(garbage) {
+				if(ch == '!') 		{ skip = true } 
+				else if(ch == '>') 	{ garbage = false } 
+				else { garbageScore += 1 }
+			} 
 		})
 
-		garbageScore
+		score
 	}
 
 }
