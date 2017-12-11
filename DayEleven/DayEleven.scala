9bf1ef86fa39cfae8636f6545c422d37c6bbe84d
James Collerton <James.Collerton@kobaltmusic.com>
2017-12-11 13:39:31 +0000

    Part two done

--- DayEleven/DayEleven.scala
+++ DayEleven/DayEleven.scala
@@ -10,14 +10,13 @@ object DayEleven {
 	// Using https://www.redblobgames.com/grids/hexagons/
 	// and cube coordinates
 	def solveOne(input: Array[String]): Unit = {
-		val(x, y, z) = solveOneStep(input, (0, 0, 0))
-		val maxDistance = (Math.abs(x) + Math.abs(y) + Math.abs(z)) / 2
+		val maxDistance = solveOneStep(input, (0, 0, 0), 0)
 		println("Max distance is " + maxDistance)
 	}
 
-	@annotation.tailrec
-	def solveOneStep(input: Array[String], coords: (Int, Int, Int)): (Int, Int, Int) = {
-		if(input.length <= 0) return coords
+        @annotation.tailrec
+	def solveOneStep(input: Array[String], coords: (Int, Int, Int), maxDistance: Int): Int = {
+                if(input.length <= 0) return maxDistance
 		val newCoords = input(0) match {
 			case "nw" 	=> (coords._1 - 1, coords._2 + 1, coords._3)
 			case "n" 	=> (coords._1, coords._2 + 1, coords._3 - 1)
@@ -26,7 +25,8 @@ object DayEleven {
 			case "s" 	=> (coords._1, coords._2 - 1, coords._3 + 1)
 			case "sw" 	=> (coords._1 - 1, coords._2, coords._3 + 1)
 		}
-		solveOneStep(input.tail, newCoords)
+		val newDistance = (Math.abs(newCoords._1) + Math.abs(newCoords._2) + Math.abs(newCoords._3)) / 2
+		solveOneStep(input.tail, newCoords, newDistance.max(maxDistance))  
 	}
 
 }
