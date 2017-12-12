45ac759df21376ddbd81615b9bf60818c1a33e64
James Collerton <jamescollerton@live.co.uk>
2017-12-12 18:31:44 +0000

    Finished part 2

--- DayTwelve/DayTwelve.scala
+++ DayTwelve/DayTwelve.scala
@@ -6,11 +6,24 @@ object DayTwelve {
 
 	def main(args: Array[String]) = {
 		val input = Utils.readIn("DayTwelveInput.txt")
-		solveOne(input)	
+		// solveOne(input)	
+		solveTwo(input)
 	}
 
 	def solveOne(programs: HashMap[Int, Array[Int]]): Unit = {
 		println("Number in group " + solveOneStep(0, programs, new Array(0)).length)
+	}
+
+	def solveTwo(programs: HashMap[Int, Array[Int]]): Unit = {
+		val remainingPrograms = programs.keySet.toArray
+		val allPrograms = programs.keySet.toArray
+		println("Number of groups " + (solveTwoStep(programs, remainingPrograms) - 1))
+	}
+
+	def solveTwoStep(programs: HashMap[Int, Array[Int]], remainingPrograms: Array[Int]): Int = {
+		if(remainingPrograms.length <= 0) return 0
+		val newlyVisited = solveOneStep(remainingPrograms(0), programs, new Array(remainingPrograms(0)))
+		solveTwoStep(programs, remainingPrograms diff newlyVisited) + 1
 	}
 
 	def solveOneStep(currProg: Int, programs: HashMap[Int, Array[Int]], visitedPrograms: Array[Int]): Array[Int] = {
