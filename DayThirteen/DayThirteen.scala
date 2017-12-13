82893025432262b27e6f7c5cd7bc11eac18e3f46
James Collerton <James.Collerton@kobaltmusic.com>
2017-12-13 13:42:46 +0000

    Solved part one

--- DayThirteen/DayThirteen.scala
+++ DayThirteen/DayThirteen.scala
@@ -1,30 +1,56 @@
 import Utils._
 import scala.collection.mutable.HashMap
-
+                
 object DayThirteen {
 
 	def main(args: Array[String]): Unit = {
-		val input = Utils.readIn("DayThirteenTestInputOne.txt")
+		val input = Utils.readIn("DayThirteenInput.txt")
 		solveOne(input)
 	}
 
-	def solveOne(fireWall: HashMap[Int, (Int, Int)]): Unit = {
+	def solveOne(fireWall: HashMap[Int, (Int, Int, Boolean)]): Unit = {
 		val stopPosition = fireWall.map{ case(k, v) => k }.toArray.max
 		println("Severity " + solveOneStep(0, fireWall, stopPosition))
 	}
 
-	def solveOneStep(currPos: Int, fireWall: HashMap[Int, (Int, Int)], stopPosition: Int): Int = {
+	def solveOneStep(currPos: Int, fireWall: HashMap[Int, (Int, Int, Boolean)], stopPosition: Int): Int = {
 		if(currPos > stopPosition) return 0
-		val movedFireWall = fireWall.map{case(k, v)  => {
-			// Need to implement flag for going back and forwards
-			if(v._2 == v._1)
-			if(v._2 == 0)
-			(k, (v._1,  v._2 + 1))
+
+		// Check if we're caught
+		val caughtIncrement = if(fireWall.contains(currPos) && fireWall(currPos)._2 == 1) { 
+			currPos * fireWall(currPos)._1
+		} else {
+			0
 		}
-		if(movedFirewall.contains(currPos) && movedFirewall(currPos)._2 == 1) {
-			solveOneStep(currPos + 1, movedFireWall, stopPosition) + currPos * movedFirewall(currPos)._1
-		}
-		solveOneStep(currPos + 1, movedFireWall, stopPosition) 
+
+		val movedFireWall = fireWall
+		fireWall.foreach{case(k, v)  => {
+                        // Going forwards
+                        if(v._3 == false) {
+				// If we are at the end
+				if(v._2 == v._1) {
+					movedFireWall(k) = (v._1, v._2 - 1, true)
+				} else {
+					movedFireWall(k) = (v._1, v._2 + 1, v._3)
+				}             
+                        // Going backwards
+                        } else {
+				// If we are at the start
+				if(v._2 == 1) {
+					movedFireWall(k) = (v._1, v._2 + 1, false)
+				} else {
+					movedFireWall(k) = (v._1, v._2 - 1, v._3)
+				}
+                        }
+		}}
+
+		//println()
+		//println("" + currPos)
+
+		//println()
+		//movedFireWall.foreach{ case(k, v) => println("Key " + k  + " Value " + v._1 + ", " + v._2 + ", " + v._3) }
+			
+		solveOneStep(currPos + 1, movedFireWall, stopPosition) + caughtIncrement
 	}
 
 }
--- DayThirteen/Utils.scala
+++ DayThirteen/Utils.scala
@@ -10,23 +10,23 @@ object Utils {
 
 	val FILE_LOCATION = "Input/"
 
-	def readIn(fileName: String): HashMap[Int, (Int, Int)] = {
+	def readIn(fileName: String): HashMap[Int, (Int, Int, Boolean)] = {
 
-		def readRows(resource: Source): HashMap[Int, (Int, Int)] = {
+		def readRows(resource: Source): HashMap[Int, (Int, Int, Boolean)] = {
 
-			val rows = new HashMap[Int, (Int, Int)]	
+			val rows = new HashMap[Int, (Int, Int, Boolean)]	
 
 			for (line <- resource.getLines) {
-				rows(line.split(":")(0).trim.toInt) = (line.split(":")(1).trim.toInt, 0)
+				rows(line.split(":")(0).trim.toInt) = (line.split(":")(1).trim.toInt, 1, false)
 			}
 
-			printRows(rows)
+			//printRows(rows)
 
 			rows
 		}
 
-		def printRows(rows: HashMap[Int, (Int, Int)]): Unit = {
-			rows.foreach{ case (key, value) => println("Key " + key + " Values " + value._1 + " " + value._2) }
+		def printRows(rows: HashMap[Int, (Int, Int, Boolean)]): Unit = {
+			rows.foreach{ case (key, value) => println("Key " + key + " Values " + value._1 + " " + value._2 + " " + value._3) }
 		}
 
 		def using[A <: { def close(): Unit }, B](resource: A)(f: A => B): B = {
