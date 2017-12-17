b6ec7b5d0cbefce64694f870ce8e9afa90a52c23
James Collerton <jamescollerton@live.co.uk>
2017-12-17 11:12:03 +0000

    Part two done

--- DaySeventeen/DaySeventeen.scala
+++ DaySeventeen/DaySeventeen.scala
@@ -5,35 +5,26 @@ object DaySeventeen {
 	def main(args: Array[String]): Unit = {
 		solveOne()
 	}
-	
+
 	def solveOne(): Unit = {
 		val step = 356;
-		println("Solve step " + solveOneStep(ArrayBuffer(0), 0, 1, step))
+		println("Solve step " + solveOneStep(0, 0, 1, step))
 	}
-
+	
 	@annotation.tailrec
-	def solveOneStep(buffer: ArrayBuffer[Int], currPos: Int, currValue: Int, step: Int): ArrayBuffer[Int] = {
-		if(currValue == 1000000) return buffer
-		//println()
-		//println("Current value " + currValue)
-		val modStep = step % buffer.length
-		//println("Mod step " + modStep)
-		//println("Curr pos " + currPos)
-		val nextPosition = findNextPosition(buffer.length, currPos, modStep)
-		//println("Next position " + nextPosition)
-		//if(nextPosition > buffer.length - 1) buffer += 0
-		buffer.insert(nextPosition, currValue)
-		//println("New buffer " + buffer.mkString(","))
-		solveOneStep(buffer, nextPosition, currValue + 1, step)
+	def solveOneStep(currFirstValue: Int, currPos: Int, currValue: Int, step: Int): Int = {
+		if(currValue == 50000000) return currFirstValue
+		val modStep = step % currValue
+		val nextPosition = findNextPosition(currValue, currPos, modStep)
+		val newFirstValue = if(nextPosition == 1) currValue else currFirstValue
+		solveOneStep(newFirstValue, nextPosition, currValue + 1, step)
 	}
 
 	@annotation.tailrec
 	def findNextPosition(bufferLength: Int, currPos: Int, modStep: Int) : Int = {
 		if(currPos + modStep < bufferLength) return currPos + modStep + 1
 		val toEnd = bufferLength - 1 - currPos
-		//println("To End " + toEnd)
 		val remainingStep = modStep - toEnd - 1
-		//println("Remaining step " + remainingStep)
 		findNextPosition(bufferLength, 0, remainingStep)
 	}
 
