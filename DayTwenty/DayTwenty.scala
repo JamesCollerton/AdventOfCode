e82be24e238dfdc5b4b827945d64317cf33c2033
James Collerton <James.Collerton@kobaltmusic.com>
2017-12-20 12:52:19 +0000

    Solve part one

--- DayTwenty/DayTwenty.scala
+++ DayTwenty/DayTwenty.scala
@@ -1,10 +1,21 @@
 import Utils._
 import ThreeDVector._
 
+import scala.collection.mutable.ArrayBuffer
+
 object DayTwenty {
 
 	def main(args: Array[String]): Unit = {
 		val input = Utils.readIn("DayTwentyInput.txt")
+		solveOne(input)
+	}
+
+	def solveOne(propertiesVectors: ArrayBuffer[PropertiesVector]): Unit = {
+	
+		// Index of particle with lowest acceleration. Fluke because there's only one
+		// particle with min acceleration.	
+		println("Min acceleration " + propertiesVectors.map(_.getAcceleration()).zipWithIndex.min._2)
+
 	}
 
 }
