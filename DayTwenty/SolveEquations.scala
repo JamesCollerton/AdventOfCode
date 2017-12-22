77a9407e09c2e99a948e55768f10ac1bf3f46d83
James Collerton <jamescollerton@live.co.uk>
2017-12-22 11:14:23 +0000

    Did a stream but probably wrong approach

--- DayTwenty/SolveEquations.scala
+++ DayTwenty/SolveEquations.scala
@@ -6,60 +6,56 @@ import scala.collection.mutable.ArrayBuffer
 
 object SolveEquations {
 
-	def solveSingleEquation(a1: Double, a2: Double, v1: Double, v2: Double, p1: Double, p2: Double): ArrayBuffer[Double] = {
+	def solveSingleEquation(a1: Double, a2: Double, v1: Double, v2: Double, p1: Double, p2: Double): Stream[Double] = {
+		println("a1 " + a1 + ", a2 " + a2 + ", v1 " + v1 + ", v2 " + v2 + ", p1 " + p1 + ", p2 " + p2)
 		// Solve quadratic equation
-		if(a1 != 0 || a2 != 0) {
+		if(a1 != 0.0 || a2 != 0.0) {
 			val a = 0.5 * a1 - 0.5 * a2
 			val b = 0.5 * (a1 - a2) + (v1 - v2)
 			val c = p1 - p2
 			val tPos = (-b + Math.sqrt(Math.pow(b,2) - 4 * a * c)) / (2 * a)
 			val tNeg = (-b - Math.sqrt(Math.pow(b,2) - 4 * a * c)) / (2 * a)
-			ArrayBuffer(tPos, tNeg)
+			tPos #:: tNeg #:: Stream.empty
 		// Solve linear equation
+		} else if ((v1 - v2) != 0.0){
+			println("In linear")
+			val t = (p2 - p1)/ (v1 - v2)
+			t #:: Stream.empty
+		// See if in same plane
+		} else if (p1 == p2){
+			println("In stationary")
+			println("Here")
+			val infiniteTimeStream: Stream[Double] = {
+  				def loop(v: Double): Stream[Double] = v #:: loop(v + 1)
+  				loop(0)
+			}		
+			infiniteTimeStream
 		} else {
-			val t = (p2 - p1)/ (v2 - v1)
-			ArrayBuffer(t)
+			Stream.Empty
 		}
+
 	}
 
 	def solveQuadratic(vecOne: PropertiesVector, vecTwo: PropertiesVector, collisions: LinkedHashMap[Int, ArrayBuffer[Int]], i: Int, j: Int): Unit =  {
 
-		def aFunc(a1: Double, a2: Double, v1: Double, v2: Double, p1: Double, p2: Double): Double = {
-			0.5 * a1 - 0.5 * a2
-		}
-		val a = CoefficientCalculator.calculateCoefficient(vecOne, vecTwo)(aFunc)
-	
-		def bFunc(a1: Double, a2: Double, v1: Double, v2: Double, p1: Double, p2: Double): Double = {
-			0.5 * (a1 - a2) + (v1 - v2)
-		}
-		val b = CoefficientCalculator.calculateCoefficient(vecOne, vecTwo)(bFunc)
+		val xCollisionTimes = solveSingleEquation(vecOne.a.x, vecTwo.a.x, vecOne.v.x, vecTwo.v.x, vecOne.p.x, vecTwo.p.x)
+		val yCollisionTimes = solveSingleEquation(vecOne.a.y, vecTwo.a.y, vecOne.v.y, vecTwo.v.y, vecOne.p.y, vecTwo.p.y)
+		val zCollisionTimes = solveSingleEquation(vecOne.a.z, vecTwo.a.z, vecOne.v.z, vecTwo.v.z, vecOne.p.z, vecTwo.p.z)
 
-		def cFunc(a1: Double, a2: Double, v1: Double, v2: Double, p1: Double, p2: Double): Double = {
-			p1 - p2
-		}
-		val c = CoefficientCalculator.calculateCoefficient(vecOne, vecTwo)(cFunc)
+		println()
+		//println(yCollisionTimes.length)
+		println("X " + xCollisionTimes.mkString(", "))
+		println("Y " + yCollisionTimes.mkString(", "))
+		println("Z " + zCollisionTimes.mkString(", "))
 
-		def posSolnFunc(a: Double, b: Double, c: Double): Double = {
-			(-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a)
-		}
-		val solnPos = CoefficientCalculator.calculateSolution(a, b, c)(posSolnFunc)
-
-		def negSolnFunc(a: Double, b: Double, c: Double): Double = {
-			(-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a)
-		}
-		val solnNeg = CoefficientCalculator.calculateSolution(a, b, c)(negSolnFunc)
-		
-		println(solnPos.x + ", " + solnPos.y + ", " + solnPos.z);
-		println(solnNeg.x + ", " + solnNeg.y + ", " + solnNeg.z);
-	
-		if(solnPos.x == solnPos.y && solnPos.y == solnPos.z && !solnPos.x.isInfinite && solnPos.x >= 0) {
-			if(!collisions.contains(solnPos.x.toInt)) collisions(solnPos.x.toInt) = new ArrayBuffer[Int]()
-			collisions(solnPos.x.toInt) += i
-			collisions(solnPos.x.toInt) += j
-		} else if(solnNeg.x == solnNeg.y && solnNeg.y == solnNeg.z && !solnNeg.x.isInfinite && solnNeg.x >= 0) {
-			if(!collisions.contains(solnNeg.x.toInt)) collisions(solnNeg.x.toInt) = new ArrayBuffer[Int]()
-			collisions(solnNeg.x.toInt) += i
-			collisions(solnNeg.x.toInt) += j
+		// If we intersect on all three planes at once
+		val intersectTimes = xCollisionTimes.intersect(yCollisionTimes.intersect(zCollisionTimes)) 
+		if(intersectTimes.length > 0) {
+			intersectTimes.foreach(t => {
+				println(t)
+				collisions(t.toInt) += i
+				collisions(t.toInt) += j
+			})
 		}
 
 	}
