9f7a3672e4ed9b6a1158c4e9888f05c897f05375
James Collerton <jamescollerton@live.co.uk>
2017-12-24 19:57:07 +0000

    Done part one

--- DayTwentyTwo/DayTwentyTwo.scala
+++ DayTwentyTwo/DayTwentyTwo.scala
@@ -6,7 +6,7 @@ import scala.collection.mutable.ArrayBuffer
 object DayTwentyTwo {
 
 	def main(args: Array[String]): Unit = {
-		val input = Utils.readIn("DayTwentyTwoTestInput.txt")
+		val input = Utils.readIn("DayTwentyTwoInput.txt")
 		solveOne(input)
 	}
 
@@ -16,24 +16,58 @@ object DayTwentyTwo {
 		// Middle
 		val startingCoordinates = (grid(0).length / 2, grid.length /2)
 		// Start Moving
-		solveOneStep(infectedCoordinates, startingCoordinates, Direction.North)
+		println("Number of infected " + solveOneStep(infectedCoordinates, startingCoordinates, 0, 10000, Direction.North))
 	}
 
 	@annotation.tailrec
-	def solveOneStep(infectedCoordinates: ArrayBuffer[(Int, Int)], currentCoordinates: (Int, Int), infectionCounter: Int, counter: Int, direction: Direction): Int = {
-		return infectionCounter
+	def solveOneStep(infectedCoordinates: ArrayBuffer[(Int, Int)], currentCoordinates: (Int, Int), infectionCounter: Int, counter: Int, direction: Direction.Value): Int = {
+		if (counter == 0) return infectionCounter
+
+//		println()
+//		println("---------------------------------------------------------------")
+//		println()
+//
+//		println("Infected Coordinates")
+//		infectedCoordinates.foreach(coord => println(coord))
+//		println("Current Coordinates " + currentCoordinates)
+//		println("Infection counter " + infectionCounter)
+//		println("Counter " + counter)
+//		println("Direction " + direction)
+
 		val (newCoordinates, newInfectionCounter, newDirection) = if(infectedCoordinates.contains(currentCoordinates)) {
+			// Clean square
+			infectedCoordinates -= currentCoordinates
+
+//			println("Current Node is Infected")
+
 			// Turn right
 			val results = direction match {
-				case North => 	((currentCoordinates._1 + 1, currentCoordinates._2), infectionCounter + 1, Direction.East)
-				case East => 	((currentCoordinates._1, currentCoordinates._2 + 1), infectionCounter + 1, Direction.South)
-				case South => 	((currentCoordinates._1 - 1, currentCoordinates._2), infectionCounter + 1, Direction.West)
-				case West => 	((currentCoordinates._1, currentCoordinates._2 - 1), infectionCounter + 1, Direction.North)  
+				case Direction.North => ((currentCoordinates._1 + 1, currentCoordinates._2), infectionCounter, Direction.East)
+				case Direction.East => 	((currentCoordinates._1, currentCoordinates._2 + 1), infectionCounter, Direction.South)
+				case Direction.South => ((currentCoordinates._1 - 1, currentCoordinates._2), infectionCounter, Direction.West)
+				case Direction.West => 	((currentCoordinates._1, currentCoordinates._2 - 1), infectionCounter, Direction.North)  
 			}
 			results 
 		} else {
-			(currentCoordinates, infectionCounter, direction)
+			// Infect square	
+			infectedCoordinates += currentCoordinates
+
+//			println("Current Node is Clean")
+
+			// Turn left
+			val results = direction match {
+				case Direction.North => ((currentCoordinates._1 - 1, currentCoordinates._2), infectionCounter + 1, Direction.West)
+				case Direction.East => 	((currentCoordinates._1, currentCoordinates._2 - 1), infectionCounter + 1, Direction.North)
+				case Direction.South => ((currentCoordinates._1 + 1, currentCoordinates._2), infectionCounter + 1, Direction.East)
+				case Direction.West => 	((currentCoordinates._1, currentCoordinates._2 + 1), infectionCounter + 1, Direction.South)  
+			}
+			results 
 		}
+
+//		println()
+//		println("---------------------------------------------------------------")
+//		println()
+
 		solveOneStep(infectedCoordinates, newCoordinates, newInfectionCounter, counter - 1, newDirection)
 	} 
 
@@ -44,6 +78,7 @@ object DayTwentyTwo {
 				else None
 			} } 
 		} }
+		infectedCoords.foreach(coord => println(coord))
 		infectedCoords
 	}
 
