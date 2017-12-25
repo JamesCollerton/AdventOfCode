4d399797930f5e1753332eb380ae6daf387664d2
James Collerton <jamescollerton@live.co.uk>
2017-12-25 12:09:36 +0000

    Works, needs to be more efficient

--- DayTwentyTwo/DayTwentyTwo.scala
+++ DayTwentyTwo/DayTwentyTwo.scala
@@ -16,12 +16,14 @@ object DayTwentyTwo {
 		// Middle
 		val startingCoordinates = (grid(0).length / 2, grid.length /2)
 		// Start Moving
-		println("Number of infected " + solveOneStep(infectedCoordinates, startingCoordinates, 0, 10000, Direction.North))
+		println("Number of infected " + solveOneStep(infectedCoordinates, startingCoordinates, 0, 10000000, Direction.North))
 	}
 
 	@annotation.tailrec
-	def solveOneStep(infectedCoordinates: ArrayBuffer[(Int, Int)], currentCoordinates: (Int, Int), infectionCounter: Int, counter: Int, direction: Direction.Value): Int = {
+	def solveOneStep(infectedCoordinates: ArrayBuffer[(Int, Int, String)], currentCoordinates: (Int, Int), infectionCounter: Int, counter: Int, direction: Direction.Value): Int = {
 		if (counter == 0) return infectionCounter
+
+		if (counter % 10000 == 0) println(counter)
 
 //		println()
 //		println("---------------------------------------------------------------")
@@ -34,12 +36,20 @@ object DayTwentyTwo {
 //		println("Counter " + counter)
 //		println("Direction " + direction)
 
-		val (newCoordinates, newInfectionCounter, newDirection) = if(infectedCoordinates.contains(currentCoordinates)) {
-			// Clean square
-			infectedCoordinates -= currentCoordinates
+		val currCoordinatesInfected = (currentCoordinates._1, currentCoordinates._2, "I")
+		val currCoordinatesFlagged = (currentCoordinates._1, currentCoordinates._2, "F")
+		val currCoordinatesWeakened = (currentCoordinates._1, currentCoordinates._2, "W")
 
-//			println("Current Node is Infected")
+		val (newCoordinates, newInfectionCounter, newDirection) = if(infectedCoordinates.contains(currCoordinatesInfected)) {
 
+			// Square is infected 
+			// 	- Turn right
+			// 	- Flag it.
+
+			// Flag
+			infectedCoordinates -= currCoordinatesInfected
+			infectedCoordinates += currCoordinatesFlagged
+ 
 			// Turn right
 			val results = direction match {
 				case Direction.North => ((currentCoordinates._1 + 1, currentCoordinates._2), infectionCounter, Direction.East)
@@ -48,18 +58,57 @@ object DayTwentyTwo {
 				case Direction.West => 	((currentCoordinates._1, currentCoordinates._2 - 1), infectionCounter, Direction.North)  
 			}
 			results 
-		} else {
-			// Infect square	
-			infectedCoordinates += currentCoordinates
+		} else if(infectedCoordinates.contains(currCoordinatesFlagged)) {
 
-//			println("Current Node is Clean")
+			// Square is flagged
+			// 	- Turn back
+			// 	- Clean it
+			
+			// Clean (Remove from list)
+			infectedCoordinates -= currCoordinatesFlagged
+
+			// Turn Back
+			val results = direction match {
+				case Direction.North => ((currentCoordinates._1, currentCoordinates._2 + 1), infectionCounter, Direction.South)
+				case Direction.East => 	((currentCoordinates._1 - 1, currentCoordinates._2), infectionCounter, Direction.West)
+				case Direction.South => ((currentCoordinates._1, currentCoordinates._2 - 1), infectionCounter, Direction.North)
+				case Direction.West => 	((currentCoordinates._1 + 1, currentCoordinates._2), infectionCounter, Direction.East)  
+			}
+			results 
+
+		} else if(infectedCoordinates.contains(currCoordinatesWeakened)) {
+
+			// Square is weakened
+			// 	- Go forward
+			// 	- Infect it
+			
+			// Infect
+			infectedCoordinates -= currCoordinatesWeakened
+			infectedCoordinates += currCoordinatesInfected 
+
+			// Go forward
+			val results = direction match {
+				case Direction.North => ((currentCoordinates._1, currentCoordinates._2 - 1), infectionCounter + 1, Direction.North)
+				case Direction.East => 	((currentCoordinates._1 + 1, currentCoordinates._2), infectionCounter + 1, Direction.East)
+				case Direction.South => ((currentCoordinates._1, currentCoordinates._2 + 1), infectionCounter + 1, Direction.South)
+				case Direction.West => 	((currentCoordinates._1 - 1, currentCoordinates._2), infectionCounter + 1, Direction.West)  
+			}
+			results 
+
+		} else { 
+
+			// Square is clean
+			// 	- Turn left
+			// 	- Weaken it
+			
+			infectedCoordinates += currCoordinatesWeakened
 
 			// Turn left
 			val results = direction match {
-				case Direction.North => ((currentCoordinates._1 - 1, currentCoordinates._2), infectionCounter + 1, Direction.West)
-				case Direction.East => 	((currentCoordinates._1, currentCoordinates._2 - 1), infectionCounter + 1, Direction.North)
-				case Direction.South => ((currentCoordinates._1 + 1, currentCoordinates._2), infectionCounter + 1, Direction.East)
-				case Direction.West => 	((currentCoordinates._1, currentCoordinates._2 + 1), infectionCounter + 1, Direction.South)  
+				case Direction.North => ((currentCoordinates._1 - 1, currentCoordinates._2), infectionCounter, Direction.West)
+				case Direction.East => 	((currentCoordinates._1, currentCoordinates._2 - 1), infectionCounter, Direction.North)
+				case Direction.South => ((currentCoordinates._1 + 1, currentCoordinates._2), infectionCounter, Direction.East)
+				case Direction.West => 	((currentCoordinates._1, currentCoordinates._2 + 1), infectionCounter, Direction.South)  
 			}
 			results 
 		}
@@ -71,10 +120,10 @@ object DayTwentyTwo {
 		solveOneStep(infectedCoordinates, newCoordinates, newInfectionCounter, counter - 1, newDirection)
 	} 
 
-	def findInfectedCoordinates(grid: ArrayBuffer[Array[String]]): ArrayBuffer[(Int, Int)] = {
+	def findInfectedCoordinates(grid: ArrayBuffer[Array[String]]): ArrayBuffer[(Int, Int, String)] = {
 		val infectedCoords = grid.zipWithIndex.flatMap{ case (row, i) => {
 			row.zipWithIndex.flatMap{ case(field, j) => {
-				if (field == "#")  Some( (j, i) )
+				if (field == "#")  Some( (j, i, "I") )
 				else None
 			} } 
 		} }
