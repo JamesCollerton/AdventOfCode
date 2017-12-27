b78ae886b01bdef1344afbfd477a6eb05bbb09d3
James Collerton <jamescollerton@live.co.uk>
2017-12-27 09:28:36 +0000

    Need to optimise a little more

--- 23_DayTwentyThree/DayTwentyThree.scala
+++ 23_DayTwentyThree/DayTwentyThree.scala
@@ -10,7 +10,9 @@ object DayTwentyThree {
 
 	def main(args: Array[String]): Unit = {
 		val input = Utils.readIn("DayTwentyThreeInput.txt")
-		solveOne(input)	
+	//	solveOne(input)	
+		//println()
+		solveTwo(initialiseTwoRegisters())
 	}
 
 	def solveOne(instructions: ArrayBuffer[Instruction]): Unit = {
@@ -18,14 +20,131 @@ object DayTwentyThree {
 		val runnableZero = new ProgramThread(instructions) 
 		val threadZero = new Thread(runnableZero)
 		
-//		val runnableOne = new ProgramThread(1.0, instructions) 
-//		val threadOne = new Thread(runnableOne)
-
-//		runnableZero.setOtherThread(runnableOne)
-//		runnableOne.setOtherThread(runnableZero)
-		
 		threadZero.start()
-//		threadOne.start()
+
+	}
+
+	def initialiseTwoRegisters(): HashMap[String, Double] = {
+		val registers = new HashMap[String, Double]
+
+		('a' to 'h').foreach(ch => registers(ch.toString) = 0)
+
+		//set b 84
+		//set c b
+		//jnz a 2
+		//jnz 1 5
+		//mul b 100
+		//sub b -100000
+		//set c b
+		//sub c -17000
+
+		registers("b") = (84 * 100) + 100000
+		registers("c") = (84 * 100) + 100000 + 17000
+		
+		//registers("b") = 84
+		//registers("c") = 84
+
+		registers
+	}
+
+	var counter = 0
+
+	def solveTwo(registers: HashMap[String, Double]): Unit = {
+
+		//set f 1
+		//set d 2
+
+		registers("f") = 1
+		registers("d") = 2
+		registers("e") = 2
+
+		println()
+		println("Registers f, d and e initialised")
+		println()
+
+		blocks(registers)		
+
+		println("H " + registers("h"))
+
+	}
+
+	def blocks(registers: HashMap[String, Double]): Unit = {
+		blockOne(registers)
+		val blockTwoFinished = blockTwo(registers)
+		if(!blockTwoFinished) {
+			registers("e") = 2
+			blocks(registers)
+		}
+		if(!blockThree(registers)) solveTwo(registers)
+	}
+
+	@annotation.tailrec
+	def blockOne(registers: HashMap[String, Double]): Unit = {
+
+		//set g d
+		//mul g e
+		//sub g b
+		//jnz g 2
+		//set f 0
+		//sub e -1
+		//set g e
+		//sub g b
+		
+		registers("g") = registers("d") * registers("e") - registers("b")
+		if(registers("g") == 0) registers("f") = 0
+		registers("e") += 1
+		registers("g") = registers("e") - registers("b")
+	
+		println()
+		println("Block one loop gone round")
+		registers.foreach{ case(k, v) => print(" Key " + k + " Value " + v) }
+		println()
+		
+		if(registers("g") == 0) return
+
+		blockOne(registers)
+	}
+
+	def blockTwo(registers: HashMap[String, Double]): Boolean = {
+
+		//sub d -1
+		//set g d
+		//sub g b
+		//jnz g -13
+		
+		registers("d") += 1
+		registers("g") = registers("d") - registers("b")
+
+		println()
+		println("Block two loop gone round")
+		registers.foreach{ case(k, v) => print(" Key " + k + " Value " + v) }
+		println()
+
+		if(registers("g") == 0) return true
+		else false
+
+	}
+
+	def blockThree(registers: HashMap[String, Double]): Boolean = {
+
+		//jnz f 2
+		//sub h -1
+		//set g b
+		//sub g c
+		//jnz g 2
+		//jnz 1 3
+		//sub b -17
+		//jnz 1 -23
+
+		if(registers("f") == 0) registers("h") += 1
+		registers("g") = registers("b") - registers("c")
+		if(registers("g") == 0) {
+			println(registers("h"))
+			return true
+		} else {
+			registers("b") -= 17
+			return false
+		}
 
 	}
 
--- 23_DayTwentyThree/ProgramThread.scala
+++ 23_DayTwentyThree/ProgramThread.scala
@@ -9,20 +9,19 @@ import scala.collection.mutable.ArrayBuffer
 class ProgramThread(instructionsInput: ArrayBuffer[Instruction]) extends Runnable {
 
 	var programCounter = 0
-//	var isWaiting = false;
-//	var otherThread: ProgramThread = null
-	
-//	val queue = new Queue[String];
 
 	def run(): Unit = {
-		println("Thread: " + solveOneStep(HashMap("a" -> 1), 0, instructionsInput))
+		println("Thread: " + solveOneStep(HashMap("a" -> 0), 0, instructionsInput))
 	}
 
 	@annotation.tailrec
 	private def solveOneStep(registers: HashMap[String, Double], currPosition: Int, instructions: ArrayBuffer[Instruction]): Double = {
 		
 		// If we're off the edge
-		if(currPosition < 0 || currPosition >= instructions.length) return programCounter
+		if(currPosition < 0 || currPosition >= instructions.length) {
+			println("H " + registers("h"))
+			return programCounter
+		}
 
 		// Get current instruction
 		val currInstruction = instructions(currPosition)
@@ -35,52 +34,28 @@ class ProgramThread(instructionsInput: ArrayBuffer[Instruction]) extends Runnabl
 		if(registers.contains(currInstruction.register)) {
 			registers(currInstruction.register) = currInstruction.instruction match {
 				case "set" => amountValue
-				//case "add" => currentValue + amountValue
 				case "sub" => currentValue - amountValue
 				case "mul" => {
 					programCounter += 1
 					currentValue * amountValue
 				}
-				//case "mod" => currentValue % amountValue
-				//case "rcv" => getAmountValue(registers, receiveFunction())
 				case _ => registers(currInstruction.register)
 			}
 		}
-
-		// Do we end
-		//if(isWaiting && otherThread.isWaiting) {
-		//	println("" + startNum + ": " + "Current registers " + registers.map(x => "(" + x._1 +"," + x._2 +")").mkString(", ")) 
-		//	return programCounter
-		//}
-
-		// Setting last sound
-		//currInstruction.instruction match {
-		//	case "snd" => queue.enqueue(currentValue.toString)
-		//	case _ => ""
-		//}
 
 		// Setting next position
 		val nextPosition = currInstruction.instruction match {
 			case "jnz" => if(currentValue != 0) currPosition + amountValue.toInt else currPosition + 1
 			case _ => currPosition + 1
 		}
+		
+		println()	
+		currInstruction.printline()
+		registers.foreach{ case(k, v) => print(" Key " + k + " Value " + v) }
+		println()
 
 		solveOneStep(registers, nextPosition, instructions)
 	}	
-
-//	def receiveFunction(): String = {
-//		if(otherThread.queue.length > 0) {
-//			programCounter += 1
-//			isWaiting = false
-//			return otherThread.queue.dequeue()
-//		}
-//		Thread.sleep(100)
-//		isWaiting = true
-//		if(otherThread.isWaiting) {
-//			return "!"
-//		}
-//		receiveFunction()
-//	}
 
 	def getAmountValue(registers: HashMap[String, Double], amount: String): Double = {
 		val amountValue = try {
@@ -98,9 +73,5 @@ class ProgramThread(instructionsInput: ArrayBuffer[Instruction]) extends Runnabl
 		}
 		amountValue
 	}
-
-//	def setOtherThread(programThread: ProgramThread): Unit = {
-//		this.otherThread = programThread
-//	}
 
 }
