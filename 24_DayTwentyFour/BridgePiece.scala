3bd1b94ce3b10a4ec5c6ab7e3a04df393c4f3c25
James Collerton <jamescollerton@live.co.uk>
2017-12-28 14:45:02 +0000

    halfway

--- 24_DayTwentyFour/BridgePiece.scala
+++ 24_DayTwentyFour/BridgePiece.scala
@@ -1,19 +1,22 @@
 package BridgePiece
 
+import scala.collection.mutable.ArrayBuffer
+
 class BridgePiece(val endValues: Array[Int]) {
 
 	val strength = endValues.sum
 	
 	private var topValue = 0;
 	private var bottomValue = 0;
-	private var nextBridgePiece = this;
+	private var nextBridgePieces = new ArrayBuffer[BridgePiece];
 
 	def setTopValue(topValue: Int): Unit = {
 		this.topValue = topValue
 	}
 
-	def setBottomValue(bottomValue: Int): Unit = {
-		this.bottomValue = bottomValue
+	def setBottomValueFromTopValue(): Unit = {
+		val notTopValue = endValues.filter(_ != topValue)
+		this.bottomValue = if (notTopValue.length == 0) topValue else notTopValue(0)
 	}
 
 	def getTopValue(): Int = {
@@ -24,12 +27,14 @@ class BridgePiece(val endValues: Array[Int]) {
 		bottomValue
 	}
 
-	def setNextBridgePiece(nextBridgePiece: BridgePiece): Unit = {
-		this.nextBridgePiece = nextBridgePiece
+	def addNextBridgePiece(nextBridgePiece: BridgePiece): Unit = {
+		nextBridgePieces += nextBridgePiece
 	}
 
 	def print(): Unit = {
-		println(endValues(0) + "/" + endValues(1))
+		println("Values: " + endValues(0) + "/" + endValues(1))
+		println("Top value: " + topValue)
+		println("Bottom value: " + bottomValue)
 	}	
 
 }
--- 24_DayTwentyFour/DayTwentyFour.scala
+++ 24_DayTwentyFour/DayTwentyFour.scala
@@ -19,15 +19,66 @@ object DayTwentyFour {
 		// Search for a bridge piece with the lowest end value
 		val startingPiece = findStartingPiece(bridgePieces)
 
-		// Search for all brige pieces with an end value of the other end's value
+		// Remove from array buffer
+		bridgePieces -= startingPiece
 
+		// Search for all bridge pieces with an end value of the other end's value
 		// Add all of them to the tree
-
 		// Remove them from the arraybuffer
+		makeBranches(startingPiece, bridgePieces)
+
+		
 
 		// Repeat
 
 		// Will return an array buffer of all of the starting nodes for the tree
+
+	}
+
+	def makeBranches(currentPiece: BridgePiece, bridgePieces: ArrayBuffer[BridgePiece]): Unit = {
+		
+		println()
+		println("--------------------------------------------")
+		println()
+		println("Finding All Branches for Current Piece")	
+		currentPiece.print()
+		
+		// All indexes for each end value
+		val allEndValues = bridgePieces.zipWithIndex.flatMap{ case (bp, idx) => bp.endValues.map(ev => (idx, ev)) }
+		
+		// Indexes of each of the next pieces matching the bottom value
+		val nextPiecesIndexes = allEndValues.filter(x => x._2 == currentPiece.getBottomValue()).map(_._1).distinct
+
+		// All of the next pieces by index
+		val nextPieces = nextPiecesIndexes.collect(bridgePieces)		
+		
+		println("Next piece indexes")
+		nextPiecesIndexes.foreach(x => println(x))
+		println("Next pieces")
+		nextPieces.foreach(_.print())
+
+		nextPieces.foreach(bridgePiece => {
+			// Set top values	
+			bridgePiece.setTopValue(currentPiece.getBottomValue())
+
+			// Set bottom values
+			bridgePiece.setBottomValueFromTopValue()
+
+			// Make next branch
+			currentPiece.addNextBridgePiece(bridgePiece)
+
+			// Remove from array buffer and recurse up
+			val tempBridgePieces = bridgePieces.clone
+			tempBridgePieces -= bridgePiece
+			makeBranches(bridgePiece, tempBridgePieces)
+
+			// Remove permanently from array buffer
+			bridgePieces -= bridgePiece
+		})
+
+		println()
+		println("--------------------------------------------")
+		println()
 
 	}
 
@@ -53,7 +104,9 @@ object DayTwentyFour {
 		minPiece.print()
 
 		minPiece.setTopValue(minPiece.endValues.min)
-		minPiece.setBottomValue(minPiece.endValues.max)
+		minPiece.setBottomValueFromTopValue()
+
+		minPiece.print()
 
 		println()
 		println("--------------------------------------------")
