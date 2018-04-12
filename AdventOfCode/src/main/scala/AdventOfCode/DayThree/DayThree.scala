e1534986dcfa936955bfbca8d3dbcf0a5bfece2f
James Collerton <jamescollerton@live.co.uk>
2018-04-12 23:06:44 +0100

    Still fiddling, but got tests set up

--- AdventOfCode/src/main/scala/AdventOfCode/DayThree/DayThree.scala
+++ AdventOfCode/src/main/scala/AdventOfCode/DayThree/DayThree.scala
@@ -6,4 +6,23 @@ object DayThree {
 
   }
 
+  def partOne(input: Int): Int = {
+    if(input == 1) return 1
+
+    getCoordinates(input)
+
+    -1
+  }
+
+  def getCoordinates(input: Int): Int = {
+    val upperBoundSquare = Math.sqrt(input).toInt + 1
+    if(input > upperBoundSquare - 1
+    val remainder = (upperBoundSquare - input) / (upperBoundSquare - 1)
+
+  }
+
+  def partTwo(input: Int): Int = {
+    -1
+  }
+
 }
--- AdventOfCode/src/test/scala/AdventOfCode/DayThree/DayThreeTest.scala
+++ AdventOfCode/src/test/scala/AdventOfCode/DayThree/DayThreeTest.scala
@@ -6,6 +6,19 @@ import org.scalatest.FunSuite
 class DayThreeTest extends FunSuite {
 
   test("Day Three Part One 1 is 0 steps") {
+    assert(DayThree.partOne(1) == 0)
+  }
+
+  test("Day Three Part One 12 is 3 steps") {
+    assert(DayThree.partOne(12) == 3)
+  }
+
+  test("Day Three Part One 23 is 2 steps") {
+    assert(DayThree.partOne(23) == 2)
+  }
+
+  test("Day Three Part One 1024 is 31 steps") {
+    assert(DayThree.partOne(1024) == 31)
   }
 
 }
