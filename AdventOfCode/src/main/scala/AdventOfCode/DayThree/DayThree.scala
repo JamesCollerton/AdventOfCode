c763af1f0b825f564218c45b276c838be8013628
James Collerton <jamescollerton@live.co.uk>
2018-04-13 08:28:07 +0100

    Worked out a coordinate system

--- AdventOfCode/src/main/scala/AdventOfCode/DayThree/DayThree.scala
+++ AdventOfCode/src/main/scala/AdventOfCode/DayThree/DayThree.scala
@@ -14,11 +14,34 @@ object DayThree {
     -1
   }
 
-  def getCoordinates(input: Int): Int = {
-    val upperBoundSquare = Math.sqrt(input).toInt + 1
-    if(input > upperBoundSquare - 1
-    val remainder = (upperBoundSquare - input) / (upperBoundSquare - 1)
+  def getCoordinates(input: Int): (Int, Int) = {
 
+    val root = Math.sqrt(input).toInt
+
+    val upperBoundRoot = if((input == root * root) && input % 2 == 1) {
+      root
+    }else if(root % 2 == 1) {
+      root + 2
+    } else {
+      root + 1
+    }
+
+    val upperBoundSquare = upperBoundRoot * upperBoundRoot
+    val sideLength = upperBoundRoot - 1
+    val sideInt = (upperBoundSquare - input) / (sideLength)
+    val cornerDistance = (upperBoundSquare - sideInt * sideLength) - input
+
+    val coordinates = if(sideInt == 0) {
+      (sideLength - cornerDistance, 0)
+    } else if(sideInt == 1) {
+      (0, cornerDistance)
+    } else if(sideInt == 2) {
+      (cornerDistance, sideLength)
+    } else {
+      (sideLength, sideLength - cornerDistance)
+    }
+
+    coordinates
   }
 
   def partTwo(input: Int): Int = {
--- AdventOfCode/src/test/scala/AdventOfCode/DayThree/DayThreeTest.scala
+++ AdventOfCode/src/test/scala/AdventOfCode/DayThree/DayThreeTest.scala
@@ -21,4 +21,35 @@ class DayThreeTest extends FunSuite {
     assert(DayThree.partOne(1024) == 31)
   }
 
+  test("Day Three getCoordinates 25 is (4, 0)") {
+    assert(DayThree.getCoordinates(25) == (4, 0))
+  }
+
+  test("Day Three getCoordinates 21 is (0, 0)") {
+    assert(DayThree.getCoordinates(21) == (0, 0))
+  }
+
+  test("Day Three getCoordinates 17 is (0, 4)") {
+    assert(DayThree.getCoordinates(17) == (0, 4))
+  }
+
+  test("Day Three getCoordinates 13 is (4, 4)") {
+    assert(DayThree.getCoordinates(13) == (4, 4))
+  }
+
+  test("Day Three getCoordinates 23 is (2, 0)") {
+    assert(DayThree.getCoordinates(23) == (2, 0))
+  }
+
+  test("Day Three getCoordinates 19 is (0, 2)") {
+    assert(DayThree.getCoordinates(19) == (0, 2))
+  }
+
+  test("Day Three getCoordinates 15 is (2, 4)") {
+    assert(DayThree.getCoordinates(15) == (2, 4))
+  }
+
+  test("Day Three getCoordinates 11 is (4, 2)") {
+    assert(DayThree.getCoordinates(11) == (4, 2))
+  }
 }
