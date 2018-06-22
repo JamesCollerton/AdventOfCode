1ff5b9605f20c730e3b00e616e2daabd7805fad4
James Collerton <James.Collerton@kobaltmusic.com>
2018-06-22 16:29:33 +0100

    Did a little more of day eight

--- AdventOfCode/src/main/scala/AdventOfCode/DayEight/DayEight.scala
+++ AdventOfCode/src/main/scala/AdventOfCode/DayEight/DayEight.scala
@@ -14,6 +14,11 @@ object DayEight {
     val input = ReadFileUtils.readFileAsStringList(filename)
   }
 
+  def parseLine(line: String): Instruction = {
+    val splitLine = line.split("\\s+")
+    Instruction(splitLine(0), splitLine(1), splitLine(2).toInt, splitLine(4), splitLine(5), splitLine(6).toInt)
+  }
+
   case class Instruction(regToChange: String, action: String, checkAmount: Int, regToCheck: String, operation: String, changeAmount: Int)
 
 }
--- AdventOfCode/src/test/scala/AdventOfCode/DayEight/DayEightTest.scala
+++ AdventOfCode/src/test/scala/AdventOfCode/DayEight/DayEightTest.scala
@@ -5,8 +5,8 @@ import org.scalatest.FunSuite
 
 class DayEightTest extends FunSuite {
 
-  test("") {
-//    assert()
+  test("Parse b inc 5 if a > 1") {
+    assert(DayEight.parseLine("b inc 5 if a > 1") == DayEight.Instruction("b", "inc", 5, "a", ">", 1))
   }
 
 }
