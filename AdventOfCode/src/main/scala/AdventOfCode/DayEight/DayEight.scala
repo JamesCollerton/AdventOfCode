36158661a27b455caf048f95e988feba78e74ee4
James Collerton <jamescollerton@live.co.uk>
2018-06-24 21:01:41 +0100

    Finished day eight part one

--- AdventOfCode/src/main/scala/AdventOfCode/DayEight/DayEight.scala
+++ AdventOfCode/src/main/scala/AdventOfCode/DayEight/DayEight.scala
@@ -2,19 +2,20 @@ package AdventOfCode.DayEight
 
 import AdventOfCode.Utils.ReadFileUtils
 
+import scala.collection.immutable.HashMap
 import scala.reflect.runtime.universe
 import scala.tools.reflect.ToolBox
 
 object DayEight {
 
   def main(args: Array[String]): Unit = {
-    partOne()
+    println(partOne("/AdventOfCode/DayEight/DayEight.txt"))
   }
 
-  def partOne(): Unit = {
-    val instructions = parseInput("/AdventOfCode/DayEight/DayEight.txt")
+  def partOne(filename: String): Int = {
+    val instructions = parseInput(filename)
     val registers = createRegisters(instructions)
-    val finalRegisters = findMax(instructions, registers)
+    findAllRegisters(instructions, registers).maxBy(_._2)._2
   }
 
   def parseInput(filename: String): List[Instruction] = {
@@ -26,19 +27,35 @@ object DayEight {
     Instruction(splitLine(0), splitLine(1), splitLine(2).toInt, splitLine(4), splitLine(5), splitLine(6).toInt)
   }
 
-  def createRegisters(instructions: List[Instruction]): List[Register] = {
-    instructions.map(i => i.regToChange).distinct.map(reg => Register(reg, 0))
+  def createRegisters(instructions: List[Instruction]): Map[String, Int] = {
+    instructions.map(i => i.regToChange).distinct.map(reg => (reg, 0)).toMap
   }
 
-  def findMax(instructions: List[Instruction], registers: List[Register]): List[Register] = {
-    if(instructions.length == 0) {
-      return registers;
+  @annotation.tailrec
+  def findAllRegisters(instructions: List[Instruction], registers: Map[String, Int]): Map[String, Int] = {
+    if(instructions.isEmpty) {
+      return registers
     }
-    val currIns = instructions(0)
-    val valueToCompare = registers(currIns.regToCheck)
-    val shouldUpdate = runTimeExecute(valueToCompare + " " + currIns.operation + " " + currIns.checkAmount)
 
-    return registers
+    val currentInstruction = instructions.head
+    val shouldUpdate = currentInstruction.shouldUpdate(registers)
+
+    val updatedRegisters = if(shouldUpdate) {
+
+      val newValue = currentInstruction.getNewValue(registers)
+
+      registers + (currentInstruction.regToChange -> newValue)
+
+    } else {
+
+      registers
+
+    }
+
+//    updatedRegisters.foreach(println)
+
+    findAllRegisters(instructions.tail, updatedRegisters)
+
   }
 
   def runTimeExecute(code: String): Boolean = {
@@ -46,8 +63,21 @@ object DayEight {
     tb.eval(tb.parse(code)).asInstanceOf[Boolean]
   }
 
-  case class Instruction(regToChange: String, action: String, changeAmount: Int, regToCheck: String, operation: String, checkAmount: Int)
+  case class Instruction(regToChange: String, action: String, changeAmount: Int, regToCheck: String, operation: String, checkAmount: Int) {
 
-  case class Register(name: String, value: Int)
+    def shouldUpdate(registers: Map[String, Int]): Boolean = {
+      val valueToCompare = registers(regToCheck)
+      runTimeExecute(valueToCompare + " " + operation + " " + checkAmount)
+    }
+
+    def getNewValue(registers: Map[String, Int]): Int = {
+      if(action == "inc") {
+        registers(regToChange) + changeAmount
+      } else {
+        registers(regToChange) - changeAmount
+      }
+    }
+
+  }
 
 }
--- AdventOfCode/src/test/scala/AdventOfCode/DayEight/DayEightTest.scala
+++ AdventOfCode/src/test/scala/AdventOfCode/DayEight/DayEightTest.scala
@@ -12,9 +12,36 @@ class DayEightTest extends FunSuite {
     val instructionOne = DayEight.Instruction("b", "inc", 5, "a", ">", 1)
     val instructionTwo = DayEight.Instruction("a", "inc", 5, "b", ">", 1)
 
-    val registerOne = DayEight.Register("a", 0)
-    val registerTwo = DayEight.Register("b", 0)
-    assert(DayEight.createRegisters(List(instructionOne, instructionTwo)).sortBy(reg => reg.name) == List(registerOne, registerTwo).sortBy(reg => reg.name))
+    val registers = Map("a" -> 0, "b" -> 0)
+    assert(DayEight.createRegisters(List(instructionOne, instructionTwo)) == registers)
+  }
+
+  test("Should update, false") {
+    val instructionOne = DayEight.Instruction("b", "inc", 5, "a", ">=", 1)
+    val register = Map("a" -> 0)
+    assert(instructionOne.shouldUpdate(register) == false)
+  }
+
+  test("Should update, true") {
+    val instructionOne = DayEight.Instruction("b", "inc", 5, "a", "<", 1)
+    val register = Map("a" -> 0)
+    assert(instructionOne.shouldUpdate(register) == true)
+  }
+
+  test("Get new value, inc, increases correctly") {
+    val instructionOne = DayEight.Instruction("a", "inc", 5, "a", "<", 1)
+    val register = Map("a" -> -5)
+    assert(instructionOne.getNewValue(register) == 0)
+  }
+
+  test("Get new value, dec, decreases correctly") {
+    val instructionOne = DayEight.Instruction("a", "dec", 5, "a", "<", 1)
+    val register = Map("a" -> 5)
+    assert(instructionOne.getNewValue(register) == 0)
+  }
+
+  test("Test part one") {
+    assert(DayEight.partOne("/AdventOfCode/DayEight/DayEightTest.txt") == 1)
   }
 
 }
