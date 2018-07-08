a0a06a3793bf6ea404b7b89c1f41231eded0a726
James Collerton <jamescollerton@live.co.uk>
2018-07-08 15:41:01 +0100

    Day nine part one done

--- AdventOfCode/src/main/scala/AdventOfCode/DayNine/DayNine.scala
+++ AdventOfCode/src/main/scala/AdventOfCode/DayNine/DayNine.scala
@@ -2,8 +2,31 @@ package AdventOfCode.DayNine
 
 object DayNine {
 
+  /**
+    * Groups are represented with {}
+    * Garbage is represented by <> (ignore characters until the close of garbage)
+    * ! is used to cancel characters (ignore next character)
+    *
+    * @param args
+    */
   def main(args: Array[String]): Unit = {
     print("Hello, world")
+  }
+
+  def countGroups(characters: Array[String], inGarbage: Boolean, numEnclosedGroups: Int): Int = {
+
+    if(characters.length == 0) return 0
+
+    val numberGroups = (characters.head, inGarbage) match {
+      case("{", false) => countGroups(characters.tail, inGarbage, numEnclosedGroups + 1)
+      case ("}", false) => countGroups(characters.tail, inGarbage, numEnclosedGroups - 1) + numEnclosedGroups
+      case ("<", _) => countGroups(characters.tail, true, numEnclosedGroups)
+      case (">", _) => countGroups(characters.tail, false, numEnclosedGroups)
+      case ("!", _) => countGroups(characters.tail.tail, inGarbage, numEnclosedGroups)
+      case _ => countGroups(characters.tail, inGarbage, numEnclosedGroups)
+    }
+
+    numberGroups
   }
 
 }
--- AdventOfCode/src/test/scala/AdventOfCode/DayNine/DayNineTest.scala
+++ AdventOfCode/src/test/scala/AdventOfCode/DayNine/DayNineTest.scala
@@ -5,35 +5,35 @@ import org.scalatest.FunSuite
 class DayNineTest extends FunSuite {
 
   test("Part one, {}, score of 1") {
-
+    assert(DayNine.countGroups("{}".split(""), false, 0) == 1)
   }
 
   test("Part one, {{{}}}, score of 6") {
-
+    assert(DayNine.countGroups("{{{}}}".split(""), false, 0) == 6)
   }
 
   test("Part one, {{},{}}, score of 5") {
-
+    assert(DayNine.countGroups("{{},{}}".split(""), false, 0) == 5)
   }
 
   test("Part one, {{{},{},{{}}}}, score of 16") {
-
+    assert(DayNine.countGroups("{{{},{},{{}}}}".split(""), false, 0) == 16)
   }
 
   test("Part one, {<a>,<a>,<a>,<a>}, score of 1") {
-
+    assert(DayNine.countGroups("{<a>,<a>,<a>,<a>}".split(""), false, 0) == 1)
   }
 
   test("Part one, {{<ab>},{<ab>},{<ab>},{<ab>}}, score of 9") {
-
+    assert(DayNine.countGroups("{{<ab>},{<ab>},{<ab>},{<ab>}}".split(""), false, 0) == 9)
   }
 
   test("Part one, {{<!!>},{<!!>},{<!!>},{<!!>}}, score of 9") {
-
+    assert(DayNine.countGroups("{{<!!>},{<!!>},{<!!>},{<!!>}}".split(""), false, 0) == 9)
   }
 
   test("Part one, {{<a!>},{<a!>},{<a!>},{<ab>}}, score of 3") {
-
+    assert(DayNine.countGroups("{{<a!>},{<a!>},{<a!>},{<ab>}}".split(""), false, 0) == 3)
   }
 
 }
